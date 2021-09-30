42aa671fd9c876c93f7a909675debc622ccd6507
eugene <eugeny.garder@adeccogroup.com>
2021-09-30 17:13:01 +0300

    email sender integration

--- scripts/mailer/config/mailerConfig.js
+++ scripts/mailer/config/mailerConfig.js
@@ -18,9 +18,9 @@ module.exports.htmlTemplate = `
         <table id="report" width=100%>
         <thead>
                 <tr>
-                    <td style="width:10%"><B>Date Time</B></td>
+                    <td style="width:30%"><B>Date Time</B></td>
                     <td style="width:20%"><b>Severity</b></td>
-                    <td style="width:80%"><b>Message</b></td>
+                    <td style="width:60%"><b>Message</b></td>
                 </tr>
             </thead>
             <tbody>
--- scripts/rules/MigrationRulesHelper.js
+++ scripts/rules/MigrationRulesHelper.js
@@ -4,22 +4,23 @@ const MigrationRulesHelper = {
   addOutputMessage: (severity, type, path, detailedMessage) => {
     let message = `${severity}: file ${path}, change type ${type}!`;
     let emailMessage = {
-      dateTime: new Date.toString(),
+      dateTime: new Date().toISOString().slice(0, 20),
       severity: severity,
-      message: `file: ${path}<br> change type: ${type}` + detailedMessage ? ` ${detailedMessage}<br>` : "",
+      message: `file: ${path}<br> change type: ${type}`,
     };
     if (detailedMessage) {
       message += detailedMessage;
+      emailMessage.message += detailedMessage;
     }
 
     MigrationRulesHelper.outputMessages.push(message);
-    emailMessages.push(emailMessage);
-    const emailSender = require("./../mailer/mailSender");
-    emailSender.sendEmails(emailMessages);
+    MigrationRulesHelper.emailMessages.push(emailMessage);
   },
   log: () => {
     if (MigrationRulesHelper.outputMessages.length) {
       console.log(MigrationRulesHelper.outputMessages);
+      const emailSender = require("./../mailer/mailSender");
+      emailSender.sendEmails(MigrationRulesHelper.emailMessages);
     } else {
       console.log("All matched files use proper migration rules ");
     }
