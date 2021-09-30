4ed07f147228e2e3da2b9bf7867e447992e076c7
eugene <eugeny.garder@adeccogroup.com>
2021-09-30 17:04:57 +0300

    email sender integration

--- scripts/rules/MigrationRulesHelper.js
+++ scripts/rules/MigrationRulesHelper.js
@@ -1,13 +1,21 @@
 const MigrationRulesHelper = {
   outputMessages: [],
+  emailMessages: [],
   addOutputMessage: (severity, type, path, detailedMessage) => {
     let message = `${severity}: file ${path}, change type ${type}!`;
-    
+    let emailMessage = {
+      dateTime: new Date.toString(),
+      severity: severity,
+      message: `file: ${path}<br> change type: ${type}` + detailedMessage ? ` ${detailedMessage}<br>` : "",
+    };
     if (detailedMessage) {
       message += detailedMessage;
     }
 
     MigrationRulesHelper.outputMessages.push(message);
+    emailMessages.push(emailMessage);
+    const emailSender = require("./../mailer/mailSender");
+    emailSender.sendEmails(emailMessages);
   },
   log: () => {
     if (MigrationRulesHelper.outputMessages.length) {
