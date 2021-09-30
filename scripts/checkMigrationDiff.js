538c8227da28e30634c807de246744714cf5dc29
nblasko <nikola.blagojevic@adeccogroup.com>
2021-09-30 15:05:56 +0200

    strip migrations from filename

--- scripts/checkMigrationDiff.js
+++ scripts/checkMigrationDiff.js
@@ -2,35 +2,42 @@ const fs = require("fs");
 const path = require("path");
 const rules = require("./rules");
 
-const diffFilters = ['add', 'change', 'delete', 'modify', 'rename'];
+const diffFilters = ["add", "change", "delete", "modify", "rename"];
 const MIGRATIONS_DIFF_FILE_PATH_BASE = "output/migrations-diff-";
 
-const {MigrationRulesHelper, Severity} = require("./rules/MigrationRulesHelper.js");
-
+const {
+  MigrationRulesHelper,
+  Severity,
+} = require("./rules/MigrationRulesHelper.js");
 
 const run = async () => {
   const diffFileNames = [];
   for (let diffFilter of diffFilters) {
     diffFileNames.push({
       type: diffFilter,
-      path: path.join(__dirname, MIGRATIONS_DIFF_FILE_PATH_BASE + diffFilter + '.txt')
+      path: path.join(
+        __dirname,
+        MIGRATIONS_DIFF_FILE_PATH_BASE + diffFilter + ".txt"
+      ),
     });
   }
 
   for (let diffFileName of diffFileNames) {
-    if (diffFileName.type === 'add') {
+    if (diffFileName.type === "add") {
       const diff = fs.readFileSync(diffFileName.path, "utf8");
       const diffLines = diff.split(/\r?\n/);
       for (let diffLine of diffLines) {
-
-
-        console.log(diffLine);
         for (let checkRule of Object.values(rules)) {
-          await checkRule(diffLine);
+          const fileName = diffLine.split("migrations/")[1];
+          await checkRule(fileName);
         }
       }
     } else {
-      MigrationRulesHelper.addOutputMessage(Severity.CRITICAL, diffFileName.type, diffFileName.path);
+      MigrationRulesHelper.addOutputMessage(
+        Severity.CRITICAL,
+        diffFileName.type,
+        diffFileName.path
+      );
     }
   }
 };
