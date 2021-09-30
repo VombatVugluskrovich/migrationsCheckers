91d0bca94d0d728ba738cfc29c503da441e570f9
Adriano Maljkovic <adriano@sprintingsoftware.com>
2021-09-30 14:48:04 +0200

    migrations rules helper

--- scripts/checkMigrationDiff.js
+++ scripts/checkMigrationDiff.js
@@ -2,19 +2,34 @@ const fs = require("fs");
 const path = require("path");
 const rules = require("./rules");
 
-const MIGRATIONS_DIFF_FILE_PATH = path.join(
-  __dirname,
-  "output/migrations-diff.txt"
-);
+const diffFilters = ['add', 'change', 'delete', 'modify', 'rename'];
+const MIGRATIONS_DIFF_FILE_PATH_BASE = "output/migrations-diff-";
+
+const {MigrationRulesHelper, Severity} = require("./rules/MigrationRulesHelper.js");
+
 
 const run = async () => {
-  const diff = fs.readFileSync(MIGRATIONS_DIFF_FILE_PATH, "utf8");
-  const diffLines = diff.split(/\r?\n/);
-  for (let diffLine of diffLines) {
-      
-    console.log(diffLine);
-    for (let checkRule of Object.values(rules)) {
-      await checkRule(diffLine);
+  const diffFileNames = [];
+  for (let diffFilter of diffFilters) {
+    diffFileNames.add({
+      type: diffFilter,
+      path: path.join(__dirname, MIGRATIONS_DIFF_FILE_PATH_BASE + diffFilter + '.txt')
+    });
+  }
+
+  for (let diffFileName of diffFileNames) {
+    if (diffFileName.type === 'add') {
+      const diff = fs.readFileSync(diffFileName.path, "utf8");
+      const diffLines = diff.split(/\r?\n/);
+      for (let diffLine of diffLines) {
+
+        console.log(diffLine);
+        for (let checkRule of Object.values(rules)) {
+          await checkRule(diffLine);
+        }
+      }
+    } else {
+      MigrationRulesHelper.addOutputMessage(Severity.CRITICAL, diffFileName.type, diffFileName.path);
     }
   }
 };
---
+++ scripts/rules/MigrationRulesHelper.js
@@ -0,0 +1,21 @@
+const MigrationRulesHelper = {
+    outputMessages: [],
+    addOutputMessage: (severity, type, path, detailedMessage) => {
+        let message = `${severity}: file ${path}, change type ${type}!`;
+        if (detailedMessage) {
+            message += detailedMessage;
+        }
+        MigrationRulesHelper.outputMessages.add(message);
+    }
+}
+
+const Severity = {
+    CRITICAL: 'Critical',
+    WARNING: 'Warning',
+    INFO: 'Info',
+}
+
+module.exports = {
+    MigrationRulesHelper,
+    Severity,
+}
