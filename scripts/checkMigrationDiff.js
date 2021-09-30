e4eec91561d94a944a618336363854ba8e863da4
eugene <eugeny.garder@adeccogroup.com>
2021-09-30 16:48:23 +0300

    email sender added

--- scripts/checkMigrationDiff.js
+++ scripts/checkMigrationDiff.js
@@ -1,22 +1,38 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
+const mailer = require("./mailer/mailSender");
 
-const MIGRATIONS_DIFF_FILE_PATH = path.join(__dirname, 'output/migrations-diff.txt');
-const rules = [
-    () => console.log('prva'),
-    () => console.log('druga'),
-];
+const MIGRATIONS_DIFF_FILE_PATH = path.join(__dirname, "output/migrations-diff.txt");
+const rules = [() => console.log("prva"), () => console.log("druga")];
 
 const run = async () => {
-    const diff = fs.readFileSync(MIGRATIONS_DIFF_FILE_PATH, 'utf8');
-    const diffLines = diff.split(/\r?\n/);
+  //   const messages = [
+  //     {
+  //       dateTime: "01-01-2021",
+  //       severity: "error",
+  //       message: "DNA error. Please replace user and press any key",
+  //     },
+  //     {
+  //       dateTime: "01-01-2021",
+  //       severity: "warning",
+  //       message: "User might cause damage due to low IQ index",
+  //     },
+  //     {
+  //       dateTime: "01-01-2021",
+  //       severity: "info",
+  //       message: "Next time please be carefull chossing miogration user",
+  //     },
+  //   ];
+  //   mailer.sendEmails(messages);
+  const diff = fs.readFileSync(MIGRATIONS_DIFF_FILE_PATH, "utf8");
+  const diffLines = diff.split(/\r?\n/);
 
-    for (let diffLine of diffLines) {
-        console.log(diffLine);
-        for (let checkRule of rules) {
-            await checkRule(diffLine);
-        }
+  for (let diffLine of diffLines) {
+    console.log(diffLine);
+    for (let checkRule of rules) {
+      await checkRule(diffLine);
     }
-}
+  }
+};
 
 run();
---
+++ scripts/mailer/config/mailerConfig.js
@@ -0,0 +1,41 @@
+module.exports.htmlTemplate = `
+<html>
+    <body>
+        <title>
+            Migration Report
+        </title>
+        <style>
+            .error {
+                color:red
+            }
+            .info {
+                color:green
+            }
+            .warning {
+               color:yellow
+            }
+        </style>
+        <table id="report" width=100%>
+        <thead>
+                <tr>
+                    <td style="width:10%"><B>Date Time</B></td>
+                    <td style="width:20%"><b>Severity</b></td>
+                    <td style="width:80%"><b>Message</b></td>
+                </tr>
+            </thead>
+            <tbody>
+                {TR}
+            </tbody>
+        </table>
+    </body>
+</html>`;
+module.exports.emailList = [
+  {
+    email: "eugeny.garder@sprintingsoftware.com",
+    name: "Eugene Garder",
+  },
+  {
+    email: "eugeny.garder@gmail.com",
+    name: "Eugene Garder @ gmail",
+  },
+];
